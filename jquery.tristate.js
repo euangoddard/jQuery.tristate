5d420302d1f18efa27e682a9fa5ef14fa320b627
Euan Goddard <euangoddard@2degreesnetwork.com>
2014-07-15 15:19:28 +0100

    Converted plugin to work with jQuery 1.11

--- jquery.tristate.js
+++ jquery.tristate.js
@@ -17,45 +17,45 @@
                     data = $checkbox.data('tristate'),
                     initial_state = null,
                     data_tristate;
-                
+
                 if (!$checkbox.is(':checkbox')) {
                     // There's no point in making anything other than a checkbox
                     // become a tristate checkbox, so skip this:
                     return true;
                 }
-                
+
                 // Determine the initial state. First see whether the
                 // "data-tristate" attribute is set:
-                data_tristate = $checkbox.attr('data-tristate'); 
+                data_tristate = $checkbox.attr('data-tristate');
                 if (data_tristate) {
                     // Validate that this state is valid:
                     initial_state = $.tristates[data_tristate];
                 }
-                
+
                 // If the data-tristate attribute is not set or is invalid, fall
                 // back to reading the checked state of the box:
                 if (!initial_state) {
-                    initial_state = $checkbox.is(':checked') ? 
+                    initial_state = $checkbox.prop('checked') ?
                         $.tristates.checked : $.tristates.unchecked;
-                } 
-                
+                }
+
                 // Ensure that the state of the box matches the state (in case
                 // of contradictory data-tristate and checked attributes). This
                 // will also set the correct data attribute:
                 self._set_state.apply($checkbox, [initial_state]);
-                
+
                 // Override the default click handler on tristate checkboxes:
                 $checkbox.bind('click.tristate', self._cycle_state);
-    	    });
+            });
         },
         state: function (new_state) {
             // Get or set the state of the checkbox(es) specified
-            
+
             // Verify that this box has been converted to a tristate checkbox:
             if (!$(this).data('tristate')) {
-                $.error('You must first set the checkbox(es) to be a tristate checkbox before calling this method')
+                $.error('You must first set the checkbox(es) to be a tristate checkbox before calling this method');
             }
-            
+
             if (typeof new_state === 'undefined') {
                 return self._get_state.apply(this);
             } else {
@@ -66,7 +66,7 @@
             // Getter for the current state of the checkox(es) specified
             var $this = $(this),
                 current_states;
-            
+
             if ($this.length === 1) {
                 // Special case the instance of a single checkbox:
                 current_states = $this.data('tristate');
@@ -75,39 +75,39 @@
                 this.each(function (index, checkbox) {
                     var $checkbox = $(checkbox),
                         id = $checkbox.attr('id') || index;
-                    
+
                     current_states[id] = $checkbox.data('tristate');
                 });
             }
-            
+
             return current_states;
-            
+
         },
         _set_state: function (new_state) {
             // Setter for an updated state of the checkox(es) specified
-            
+
             var $checkboxes = $(this),
-                state_to_set = $.tristates[new_state]; 
-            
+                state_to_set = $.tristates[new_state];
+
             // Validate the new_state:
             if (typeof state_to_set === 'undefined') {
                 $.error('You cannot set a tristate checkbox to: ' + new_state);
             }
-            
+
             switch (state_to_set) {
                 case $.tristates.unchecked:
-                    $checkboxes.removeAttr('checked').css({opacity: 1});
+                    $checkboxes.prop('checked', false).css({opacity: 1});
                     break;
                 case $.tristates.checked:
-                    $checkboxes.attr({checked: 'checked'}).css({opacity: 1});
+                    $checkboxes.prop('checked', true).css({opacity: 1});
                     break;
                 case $.tristates.mixed:
-                    $checkboxes.attr({checked: 'checked'}).css({opacity: mixed_opacity});
+                    $checkboxes.prop('checked', true).css({opacity: mixed_opacity});
                     break;
             }
-            
+
             $checkboxes.data('tristate', state_to_set);
-            
+
         },
         _cycle_state: function (event) {
             // Cycle through the state of the checkbox. The allowed transitions are:
@@ -117,12 +117,12 @@
             // NB: Therefore it is not possible to get to a mixed state by
             // clicking on a tristate checkbox, this state can only be achieved
             // by external means.
-            
+
             event.preventDefault();
-            
+
             var $checkbox = $(this),
                 new_state;
-                
+
             // Add a timeout to ensure that the DOM has correctly been updated
             // before trying to alter the state of the checkbox:
             setTimeout(function () {
@@ -139,16 +139,16 @@
                 }
                 self._set_state.apply($checkbox, [new_state]);
             }, 13);
-            
+
             return false;
         },
         _debug: function (message) {
             // Output any arguments to the console if available:
-            window.console && console.debug.apply(console, arguments);
+            window.console && window.console.debug.apply(window.console, arguments);
         }
     },
     mixed_opacity = 0.5;
-    
+
     $.fn.tristate = function (method) {
         if (self[method] && method.substr(0, 1) !== '_') {
             return self[method].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -156,12 +156,12 @@
             return self.init.apply(this, arguments);
         } else {
             $.error('Method ' +  method + ' does not exist on jQuery.tristate');
-        }  
+        }
     };
     $.tristates = {
         'checked': 'checked',
         'unchecked': 'unchecked',
         'mixed': 'mixed'
     };
-    
+
 }));
